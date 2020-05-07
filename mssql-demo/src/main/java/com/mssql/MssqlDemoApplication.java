a233f418925ccb164d40e584fb3b48e0009554da
YanCh <2291900019@qq.com>
2020-05-07 21:02:07 +0800

    测试代码修改

--- mssql-demo/src/main/java/com/mssql/MssqlDemoApplication.java
+++ mssql-demo/src/main/java/com/mssql/MssqlDemoApplication.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import com.mssql.mapper.KqInfoMapper;
 import com.mssql.model.KqInfo;
 import com.xiaoleilu.hutool.date.DateUtil;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -22,6 +25,8 @@ import java.util.Random;
 public class MssqlDemoApplication {
     @Autowired
     KqInfoMapper kqInfoMapper;
+    @Autowired
+    SqlSessionTemplate sqlSessionTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(MssqlDemoApplication.class, args);
@@ -29,17 +34,18 @@ public class MssqlDemoApplication {
 
     @RequestMapping("add")
     public String add(){
-        List<KqInfo> kqInfoList = new ArrayList<>();
-        for (int i = 0; i < 90000; i++) {
+//        SqlSession sqlSession = sqlSessionTemplate.getSqlSessionFactory().openSession(ExecutorType.BATCH, false);
+//        KqInfoMapper userMapper = sqlSession.getMapper(KqInfoMapper.class);
+        for (int i = 0; i < 70000; i++) {
             KqInfo kqInfo = new KqInfo();
             kqInfo.setKqId(createSalt());
-            kqInfo.setKqMoney("100.000");
+            kqInfo.setKqMoney("50.000");
             kqInfo.setKqPsw("");
-            kqInfo.setKqYxq(DateUtil.parse("2026-06-06 06:06:06.000"));
+            kqInfo.setKqYxq(DateUtil.parse("2028-08-18 18:18:18.000"));
             kqInfo.setState("未核销");
             kqInfo.setFqMallName("新花城總部");
-            kqInfo.setFqCkName("");
-            kqInfo.setHxMallName("默認總部門店");
+            kqInfo.setFqCkName("默認總部門店");
+            kqInfo.setHxMallName("");
             kqInfo.setHxCkName("");
             kqInfo.setHxMoney(null);
             kqInfo.setHxPerson("");
@@ -48,14 +54,15 @@ public class MssqlDemoApplication {
             kqInfo.setCreatPerson("曾廣文");
             kqInfo.setChgTime(new Date());
             kqInfo.setChgPerson("曾廣文");
-            kqInfo.setBzMemo("100實體通用券");
+            kqInfo.setBzMemo("50實體的通用券");
             kqInfo.setHxMemo("");
-            kqInfo.setKqName("100實體通用券");
+            kqInfo.setKqName("50實體的通用券");
             kqInfo.setHxDhId("");
             kqInfo.setUseTj("全部通用");
             kqInfo.setXfMe(".0000");
             kqInfoMapper.insert(kqInfo);
         }
+//        sqlSession.commit();
         return "dsadas";
     }
 
@@ -68,7 +75,7 @@ public class MssqlDemoApplication {
                 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         StringBuilder code = new StringBuilder();
         Random r = new Random();
-        while (count < 8) {
+        while (count < 9) {
             i = Math.abs(r.nextInt(maxNum));
             if (i >= 0 && i < str.length) {
                 code.append(str[i]);
